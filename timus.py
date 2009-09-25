5c58d756f39f5d65ce967fa6bc528deb83927036
Adomas Paltanavičius <adomas@shrubberysoft.com>
2009-09-25 10:50:56 +0300

    New templates.

--- timus.py
+++ timus.py
@@ -9,6 +9,7 @@ import jinja2
 ## Configuration
 START = '09:00:00 25 Sep 2009'
 END = '12:00:00 25 Sep 2009'
+TITLE = u'Example timus contest'
 USERS = {
     # User ID => title
 }
@@ -17,6 +18,7 @@ PROBLEMS = {
 }
 WRONG_PENALTY = 20
 CRAWL_PAUSE = 5.0 # Pause between requests
+UPDATE_INTERVAL = 60 # Seconds
 
 
 class odict(dict):
@@ -71,7 +73,7 @@ def minutes(delta):
     return delta.seconds / 60
 
 
-def render(template, board, start_date):
+def get_render_context(board, start_date):
     table = dict()
     scores = dict()
     # Calculate the score by assigning penalties etc.
@@ -94,28 +96,21 @@ def render(template, board, start_date):
         return -cmp(scores[a].solved, scores[b].solved) or \
                cmp(scores[a].minutes, scores[b].minutes) or \
                cmp(USERS[a], USERS[b])
-    return template.render({
+    return {
+        'title': TITLE,
+        'date': datetime.datetime.now(),
         'users_sorted': sorted(USERS, cmp=compare),
         'problems_sorted': sorted(PROBLEMS, key=PROBLEMS.get),
         'users': USERS,
         'problems': PROBLEMS,
         'scores': scores,
         'table': table
-    })
-    # for problem in problems_sorted:
-    #     print '| %s (%d)' % (PROBLEMS[problem], problem),
-    # print
-    # for user in sorted(USERS, cmp=compare):
-    #     print USERS[user], scores[user].minutes,
-    #     for problem in problems_sorted:
-    #         print '| %s %s' % (table[user][problem].plus,
-    #                            table[user][problem].time),
-    #     print
+    }
 
 
 def main(start_url='http://acm.timus.ru/status.aspx?count=100'):
     start_date, end_date = parse_date(START), parse_date(END)
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
     seen = set() # set of seen submission IDs
     board = dict()
@@ -123,39 +118,55 @@ def main(start_url='http://acm.timus.ru/status.aspx?count=100'):
         for problem in PROBLEMS:
             board.setdefault(user, {})[problem] = odict(accepted=False, wrong=0)
 
-    url = start_url
-    seen_older = seen_newer = False
-    while not seen_older:
-        print 'Retrieving %s...' % url
-        soup = BeautifulSoup(urllib2.urlopen(url))
-        next_link, items = extract(soup)
-        url = urlparse.urljoin(url, next_link)
-        for item in items:
-            if item.date > end_date:
-                seen_newer = True
-            if item.date < start_date:
-                seen_older = True
-            if item.id not in seen and \
-               item.problem in PROBLEMS and item.user in USERS and \
-               start_date <= item.date <= end_date:
-               # Was the problem already accepted for this team?
-               if board[item.user][item.problem].accepted and \
-                  item.date > board[item.user][item.problem].accepted:
-                   # Different time might mean higher (= worse) time
-                   continue
-               # Accepted?
-               if item.status in ('Accepted',):
-                   board[item.user][item.problem].accepted = item.date
-               else:
-                   board[item.user][item.problem].wrong += 1
-               seen.add(id)
-        if not seen_older:
-            time.sleep(CRAWL_PAUSE)
+    def update():
+        url = start_url
+        seen_older = seen_newer = False
+        ## TODO: optimize the update routing so that it doesn't crawl
+        ## *all* pages every time. Only the first ones should be necessary.
+        while not seen_older:
+            print 'Retrieving %s...' % url
+            soup = BeautifulSoup(urllib2.urlopen(url))
+            next_link, items = extract(soup)
+            url = urlparse.urljoin(url, next_link)
+            for item in items:
+                if item.date > end_date:
+                    seen_newer = True
+                if item.date < start_date:
+                    seen_older = True
+                if item.id not in seen and \
+                   item.problem in PROBLEMS and item.user in USERS and \
+                   start_date <= item.date <= end_date:
+                   # Was the problem already accepted for this team?
+                   if board[item.user][item.problem].accepted and \
+                      item.date > board[item.user][item.problem].accepted:
+                       # Different time might mean higher (= worse) time
+                       continue
+                   # Accepted?
+                   if item.status in ('Accepted',):
+                       board[item.user][item.problem].accepted = item.date
+                   else:
+                       board[item.user][item.problem].wrong += 1
+                   seen.add(id)
+            if not seen_older:
+                time.sleep(CRAWL_PAUSE)
+        return not seen_newer
 
-    with open('results.html', 'w') as fp:
-        template = env.get_template('template.html')
-        print >>fp, render(template, board, start_date).encode('utf-8')
+    def output():
+        context = get_render_context(board, start_date)
+        for template in ('index.html', 'top.html', 'table.html'):
+            output_name = 'output/%s' % template
+            print 'Writing %s...' % output_name
+            with open(output_name, 'w') as fp:
+                template = env.get_template(template)
+                print >>fp, template.render(context).encode('utf-8')
 
+    while True:
+        contest_in_progress = update()
+        output()
+        if not contest_in_progress:
+            print 'Contest has ended.'
+            break
+        time.sleep(UPDATE_INTERVAL)
 
 if __name__ == '__main__':
     main()
