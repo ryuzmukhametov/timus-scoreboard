342433004ffd1bc400546074c6b5c03341299296
Adomas Paltanavičius <adomas@shrubberysoft.com>
2009-10-04 21:34:13 +0300

    Stupid setuptools can't handle Unicode in my name!

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
-# -*- coding: utf-8 *-*
 from setuptools import setup, find_packages
 
 setup(name='timus.scoreboard',
     version='0.1',
     description='Custom contest support for the Timus Online Judge',
     long_description=open('README').read(),
-    author=u'Adomas Paltanavičius',
+    author='Adomas Paltanavicius',
     author_email='adomas@shrubberysoft.com',
-    #url='http://github.com/admp/timus-scoreboard',
+    url='http://github.com/admp/timus-scoreboard',
     packages=find_packages(),
     install_requires=['BeautifulSoup==3.0.7a', 'Jinja2', 'configobj'],
     entry_points={
         'console_scripts': [
             "timus-scoreboard = timus.scoreboard:main"
         ]
-    }
-    # classifiers=[
-    #     'Development Status :: 4 - Beta',
-    # ]   
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Education'
+    ]
 )
